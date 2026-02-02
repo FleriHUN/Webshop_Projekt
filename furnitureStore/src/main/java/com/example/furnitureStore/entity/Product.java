6608635a095687aca829588b423a92e0fb41b8b4
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-02 10:15:05 +0100

    Update Product.java

--- furnitureStore/src/main/java/com/example/furnitureStore/entity/Product.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/entity/Product.java
@@ -1,4 +1,23 @@
 package com.example.furnitureStore.entity;
 
-public class Product {
-}
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Product
