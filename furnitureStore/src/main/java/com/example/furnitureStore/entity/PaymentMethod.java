fefc2edac245dde052359eff054b1c3bc22f9a83
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-02 10:10:51 +0100

    Update PaymentMethod.java

--- furnitureStore/src/main/java/com/example/furnitureStore/entity/PaymentMethod.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/entity/PaymentMethod.java
@@ -1,4 +1,36 @@
 package com.example.furnitureStore.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "payment_method")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    @Size(max = 100)
+    private String name;
+
+    //Kapcsolatok:
+    @OneToMany(mappedBy = "paymentMethod", fetch = FetchType.LAZY, cascade = {})
+    @JsonIgnore
+    private List<OrderHistory> orderHistoryList;
 }
