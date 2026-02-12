d6517e694f4320bcd9428148fb658d9b8ab787f0
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-12 12:52:29 +0100

    Update OrderService.java

--- furnitureStore/src/main/java/com/example/furnitureStore/service/OrderService.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/service/OrderService.java
@@ -1,11 +1,42 @@
 package com.example.furnitureStore.service;
 
+import com.example.furnitureStore.entity.User;
+import com.example.furnitureStore.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderHistoryRepository orderHistoryRepository;
+    private final UserRepository userRepository;
+    private final StatusRepository statusRepository;
+    private final PaymentMethodRepository paymentMethodRepository;
+    private final AddressTypeRepository addressTypeRepository;
+    private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final EmailSender emailSender;
+    private final PasswordEncoder passwordEncoder;
+
+    //kesz:
+    public ResponseEntity<Object> getOrderHistoryByUserId(Integer userId) {
+        try {
+            if (userId == null) {
+                return ResponseEntity.status(422).build();
+            }
+            User searchedUser = userRepository.getUserById(userId).orElse(null);
+            if (searchedUser == null || searchedUser.getIsDeleted()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok().body(searchedUser.getOrderHistoryList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
