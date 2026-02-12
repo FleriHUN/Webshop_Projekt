e30c8d0c74ca3a825ca626c0747c3985fe183171
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-12 12:20:38 +0100

    repo

--- furnitureStore/src/main/java/com/example/furnitureStore/repository/ProductRepository.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/repository/ProductRepository.java
@@ -2,6 +2,17 @@ package com.example.furnitureStore.repository;
 
 import com.example.furnitureStore.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Procedure(name = "getFirstThreeProduct", procedureName = "getFirstThreeProduct")
+    List<Product> getFirstThreeProduct();
+
+    @Procedure(name = "getProductById", procedureName = "getProductById")
+    Optional<Product> getProductById(@Param("idIN") Integer id);
 }
--- furnitureStore/src/main/java/com/example/furnitureStore/repository/UserRepository.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/repository/UserRepository.java
@@ -5,9 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Procedure(name = "login", procedureName = "login")
-    User login(@Param("emailIN") String email, @Param("passwordIN") String password);
+    @Procedure(name = "getUserByUsername", procedureName = "getUserByUsername")
+    Optional<User> getUserByUsername(@Param("usernameIN") String username);
+
+    @Procedure(name = "getUserById", procedureName = "getUserById")
+    Optional<User> getUserById(@Param("idIN") Integer id);
+
+    @Procedure(name = "getUserByEmail", procedureName = "getUserByEmail")
+    Optional<User> getUserByEmail(@Param("emailIN") String email);
+
+    @Procedure(name = "deleteUserById", procedureName = "deleteUserById")
+    void deleteUserById(@Param("idIN") Integer id);
 }
 
