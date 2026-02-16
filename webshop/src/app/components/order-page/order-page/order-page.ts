04a4fa7f09ffbc0ada2afaf7b9f4361e47862b38
Fleri <floriankemeny@gmail.com>
2026-02-16 10:33:47 +0100

    elmaradt orderpage commit

---
+++ webshop/src/app/components/order-page/order-page/order-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-order-page',
+  imports: [RouterModule],
+  templateUrl: './order-page.html',
+  styleUrl: './order-page.scss',
+})
+export class OrderPage {
+
+}
