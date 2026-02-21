49f3afb4e2df8d8d4de3d4f2919c76fdc94b1013
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-21 14:11:37 +0100

    Update UserController.java

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/UserController.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/UserController.java
@@ -2,27 +2,56 @@ package com.example.furnitureStore.controller;
 
 import com.example.furnitureStore.entity.User;
 import com.example.furnitureStore.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@CrossOrigin({"http://localhost:4200"})
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody Map<String, String> body) {
-        return userService.login(body.get("com/example/furnitureStore/config/email"), body.get("password"));
+    private ResponseEntity<Object> login(@RequestBody JsonNode requestBody) {
+        return userService.login(requestBody.get("username").asText(null), requestBody.get("password").asText(null));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Object> register(@RequestBody User newUser) {
+    private ResponseEntity<Object> register(@RequestBody User newUser) {
         return userService.register(newUser);
+    }
+
+    @PatchMapping("/{id}")
+    private ResponseEntity<Object> update(@RequestBody JsonNode requestBody, @PathVariable("id") Integer id) {
+        return userService.update(id, requestBody.get("username").asText(null), requestBody.get("com/example/furnitureStore/config/email").asText(null));
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Object> delete(@PathVariable("id") Integer id) {
+        return userService.delete(id);
+    }
+
+    @PatchMapping("/pfp/{id}")
+    private ResponseEntity<Object> changePfp(@RequestParam("pfpFile") MultipartFile newPfpImage, @PathVariable("id") Integer id) {
+        return userService.changePfp(newPfpImage, id);
+    }
+
+    @GetMapping("/vCode")
+    private ResponseEntity<Object> getVerificationCode(@RequestParam("com/example/furnitureStore/config/email") String email) {
+        return userService.getVerificationCode(email);
+    }
+
+    @PostMapping("/check")
+    private ResponseEntity<Object> checkVerificationCode(@RequestBody JsonNode requestBody) {
+        return userService.checkVerificationCode(requestBody.get("vCode").asText(null), requestBody.get("com/example/furnitureStore/config/email").asText(null));
+    }
+
+    @PatchMapping("/password")
+    private ResponseEntity<Object> changePassword(@RequestBody JsonNode requestBody) {
+        return userService.changePassword(requestBody.get("com/example/furnitureStore/config/email").asText(null), requestBody.get("newPassword").asText(null));
     }
 }
