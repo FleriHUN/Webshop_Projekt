f4fee929c76016e3f8c25483fdb04ffdb0c68bd0
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-21 18:40:12 +0100

    Update Ordercontroller.java

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/Ordercontroller.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/Ordercontroller.java
@@ -1,9 +1,11 @@
 package com.example.furnitureStore.controller;
 
+import com.example.furnitureStore.entity.OrderHistory;
 import com.example.furnitureStore.service.OrderService;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -11,4 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private OrderService orderService;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Object> getOrderHistoryByUserId(@PathVariable("id") Integer userId) {
+        return orderService.getOrderHistoryByUserId(userId);
+    }
+
+    @DeleteMapping("/cancel/{id}")
+    public ResponseEntity<Object> cancelOrder(@PathVariable("id") Integer orderId, @RequestBody JsonNode requestBody) {
+        return orderService.cancelOrder(orderId, requestBody.get("cancelerUserId").asInt());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Object> getOrderHistoryByVCode(@RequestParam("com/example/furnitureStore/config/email") String email, @RequestParam("vCode") String vCode) {
+        return orderService.getOrderHistoryByVCode(email, vCode);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Object> getAllOrder() {
+        return orderService.getAllOrder();
+    }
+
+    @PostMapping("/basket/{id}")
+    public ResponseEntity<Object> sendOrder(@RequestBody OrderHistory newOrder, @PathVariable("id") Integer basketId) {
+        return orderService.sendOrder(newOrder, basketId);
+    }
 }
+
