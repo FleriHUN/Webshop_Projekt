8d41b18212659584fdaf4d9fce12381d984926a9
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-21 15:54:03 +0100

    Update ReviewController.java

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/ReviewController.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/ReviewController.java
@@ -1,14 +1,31 @@
 package com.example.furnitureStore.controller;
 
+import com.example.furnitureStore.entity.Review;
 import com.example.furnitureStore.service.ReviewService;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
-    private ReviewService reviewService;
+    private final ReviewService reviewService;
+
+    @PostMapping("")
+    private ResponseEntity<Object> addReview(@RequestBody Review newReview) {
+        return reviewService.addReview(newReview);
+    }
+
+    @PutMapping("")
+    private ResponseEntity<Object> updateReview(@RequestBody JsonNode updatedReview) {
+        return reviewService.updateReview(updatedReview.get("id").asInt(0), updatedReview.get("reviewText").asText(null));
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Object> deleteReview(@PathVariable("id") Integer id) {
+        return reviewService.deleteReview(id);
+    }
 }
