fafdf63259125fe764e106a57260d6e9b235479a
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-25 13:49:21 +0100

    Update ProductController.java

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/ProductController.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/ProductController.java
@@ -1,14 +1,45 @@
 package com.example.furnitureStore.controller;
 
+import com.example.furnitureStore.entity.Brand;
+import com.example.furnitureStore.entity.Category;
+import com.example.furnitureStore.entity.Product;
 import com.example.furnitureStore.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
+
+    @GetMapping("/category/{id}")
+    private ResponseEntity<Object> getProductsByCategory(@PathVariable("id") Integer id) {
+        return productService.getProductsByCategory(id);
+    }
+
+    @GetMapping("/homePage")
+    private ResponseEntity<Object> getFirstThreeProduct() {
+        return productService.getFirstThreeProduct();
+    }
+
+    @PostMapping("")
+    private ResponseEntity<Object> addProduct(@RequestBody Product newProduct) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Object> deleteProduct(@PathVariable("id") Integer id) {
+        return productService.deleteProduct(id);
+    }
+
+    @PutMapping("")
+    private ResponseEntity<Object> updateProduct(@RequestBody Product updatedProduct) {
+        return null;
+    }
+
+
 }
