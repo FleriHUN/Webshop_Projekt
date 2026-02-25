a4146a95b11f205aa18f413274f09dd369437206
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-25 10:45:52 +0100

    Update CategoryRepository.java

--- furnitureStore/src/main/java/com/example/furnitureStore/repository/CategoryRepository.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/repository/CategoryRepository.java
@@ -2,6 +2,16 @@ package com.example.furnitureStore.repository;
 
 import com.example.furnitureStore.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    //
+    @Procedure(name = "getParentCategories", procedureName = "getParentCategories")
+    List<Category> getParentCategories();
+
+    @Procedure(name = "getSubCategoriesOfParentCategory", procedureName = "getSubCategoriesOfParentCategory")
+    List<Category> getSubCategoriesOfParentCategory(@Param("idIN") Integer id);
 }
