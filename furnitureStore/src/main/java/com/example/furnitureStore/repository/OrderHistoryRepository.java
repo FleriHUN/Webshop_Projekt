17928dc10af84fe9cf8a6efbfcee08f5d4a85395
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-25 10:29:59 +0100

    Update OrderHistoryRepository.java

--- furnitureStore/src/main/java/com/example/furnitureStore/repository/OrderHistoryRepository.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/repository/OrderHistoryRepository.java
@@ -3,6 +3,11 @@ package com.example.furnitureStore.repository;
 import com.example.furnitureStore.entity.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Integer> {
+
+    List<OrderHistory> findByEmail(String email);
 }
 
