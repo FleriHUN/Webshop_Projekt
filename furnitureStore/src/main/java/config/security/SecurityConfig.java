e0ca3085ce182ecf910044e7d8cce1d1ad005908
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-26 12:16:52 +0100

    Update SecurityConfig.java

--- furnitureStore/src/main/java/config/security/SecurityConfig.java
+++ furnitureStore/src/main/java/config/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package config.security;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,36 +21,53 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final DbUserSetter userSetter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtGeneratorFilter jwtGeneratorFilter;
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
                         config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                         config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization", "Access-Control-Expose-Headers"));
+                        config.setAllowCredentials(true);
+                        config.setExposedHeaders(Arrays.asList("Authorization", "Bearer "));
                         config.setMaxAge(3600L);
                         return config;
                     }
                 }))
-                .authorizeHttpRequests((requests) ->
-                        requests.anyRequest().permitAll()
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((request) -> request
+                        .anyRequest().permitAll()
                 )
-                .formLogin(Customizer.withDefaults())
-                .csrf(crs -> crs.disable())
-                .httpBasic(Customizer.withDefaults());
-
+//                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+//                .addFilterAfter(jwtGeneratorFilter, BasicAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder(16, 32, 1, 1 << 12, 3);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userSetter);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
