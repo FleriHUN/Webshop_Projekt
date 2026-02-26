13dcc9a5bbb7937c90e6ebdec06435c428413af3
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-26 10:25:28 +0100

    catcon, xml

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/CategoryController.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/CategoryController.java
@@ -13,10 +13,15 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
     private final CategoryService categoryService;
 
-//    @GetMapping("")
-//    private ResponseEntity<Object> getAllCategory() {
-//        return null;
-//    }
+    @GetMapping("/parents")
+    private ResponseEntity<Object> getAllParentCategories() {
+        return categoryService.getAllParentCategory();
+    }
+
+    @GetMapping("/sub/{id}")
+    private ResponseEntity<Object> getAllSubCategoryOfParentCategory(@PathVariable("id") Integer id) {
+        return categoryService.getAllSubCategory(id);
+    }
 
     @PostMapping("")
     private ResponseEntity<Object> addCategory(@RequestBody Category newCategory) {
