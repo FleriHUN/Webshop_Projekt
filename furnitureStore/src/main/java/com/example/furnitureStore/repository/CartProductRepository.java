25f2474186ffa25719058248bc7629cf6cdf27c2
TBalazs1 <balu.torok.0213@gmail.com>
2026-02-27 23:14:39 +0100

    Update CartProductRepository.java

--- furnitureStore/src/main/java/com/example/furnitureStore/repository/CartProductRepository.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/repository/CartProductRepository.java
@@ -2,6 +2,10 @@ package com.example.furnitureStore.repository;
 
 import com.example.furnitureStore.entity.CartProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Integer> {
+    @Procedure(name = "deleteProductFromCart", procedureName = "deleteProductFromCart")
+    void deleteProductFromCart(@Param("idIN") Integer productId);
 }
