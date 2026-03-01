337d308305adabe8801bd8a8c8364a3608c02f4f
TBalazs1 <balu.torok.0213@gmail.com>
2026-03-02 00:20:54 +0100

    Update BillingDetail.java

--- furnitureStore/src/main/java/com/example/furnitureStore/entity/BillingDetail.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/entity/BillingDetail.java
@@ -58,6 +58,7 @@ public class BillingDetail {
     @Null
     private String other;
 
+    //Kapcsolatok:
     @ManyToOne(cascade = {})
     @JoinColumn(name = "address_type_id")
     private AddressType billingAddressType;
