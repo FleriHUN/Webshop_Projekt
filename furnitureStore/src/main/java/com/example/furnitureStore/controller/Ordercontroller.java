3ae6fc2a2da2a0d485c8047e23e2cd711b6c5b32
TBalazs1 <balu.torok.0213@gmail.com>
2026-05-01 12:43:17 +0200

    Update Ordercontroller.java

--- furnitureStore/src/main/java/com/example/furnitureStore/controller/Ordercontroller.java
+++ furnitureStore/src/main/java/com/example/furnitureStore/controller/Ordercontroller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
-public class OrderController {
+public class Ordercontroller {
     private final OrderService orderService;
 
     @GetMapping("/user/{id}")
